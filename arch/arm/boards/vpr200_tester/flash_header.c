b85a151ba7f83d909d901ee78a10273eec486b28
Marc Reilly <marc@cpdesign.com.au>
2014-08-07 16:11:56 +1000

    vpr200_tester: add extra diagnostic helpers to flash header
    This adds compile time defines to output via UART when the
    processor is doing the DCD block. This enables us to know that
    a certain point of the boot stage was achieved.
    
    Also add a special flash header to be used in conjunction with
    the imx internal boot ROM.
    
    Signed-off-by: Marc Reilly <marc@cpdesign.com.au>

--- arch/arm/boards/vpr200_tester/flash_header.c
+++ arch/arm/boards/vpr200_tester/flash_header.c
@@ -4,10 +4,35 @@
 
 extern void exception_vectors(void);
 
+/*
+ * You can enable this at compile time and set TEXT_BASE to 0x1000d000
+ * Then, send the image via the iROM bootloader and execute from internal
+ * RAM. (note internal RAM size is 128K so barebox image needs to be less
+ * than 0x13000 bytes). (Note also stack and heap default to be before 
+ * TEXT_BASE. So far, used 0x4000 and 0x8000 respecively, OK)
+ *
+ * This header must be at the download position
+ */
+#define RUNNABLE_FROM_IROM_BOOTLOADER 0
+
+#if RUNNABLE_FROM_IROM_BOOTLOADER
+struct imx_flash_header __flash_header_start test_flash_header = {
+	.app_code_jump_vector	=((unsigned int)&exception_vectors),
+	.app_code_barker	= 0,
+	.app_code_csf		= 0,
+	.dcd_ptr_ptr		= TEXT_BASE + offsetof(struct imx_flash_header, dcd),
+	.super_root_key		= 0,
+	.dcd			= 0,
+	.app_dest		= 0,
+	.dcd_barker		= 0,
+	.dcd_block_len	= 0,
+};
+#else
 void __naked __flash_header_start go(void)
 {
 	__asm__ __volatile__("b exception_vectors\n");
 }
+#endif
 
 /*
 * This define can be set to 0 to skip initialization of the first mem
@@ -69,7 +94,30 @@ struct imx_flash_header __flash_header_0x1000 nor_flash_header = {
 
 unsigned long __image_len_0x1000 nor_barebox_len = 0x40000;
 
+/*
+ * This will poke the UART1 registers to output a string
+ * as the DCD entries are processed.
+ */
+#define FLASH_HEADER_UART_TX_STARTUP 1
+
 struct imx_dcd_entry __dcd_entry_0x0400 sd_dcd_entry[] = {
+#if FLASH_HEADER_UART_TX_STARTUP
+	{ .ptr_type = 4, .addr = 0x43F90080, .val = 0x00000001, }, //UCR1
+	{ .ptr_type = 4, .addr = 0x43F90084, .val = 0x00004025, },
+	{ .ptr_type = 4, .addr = 0x43F90090, .val = 0x00000a81, }, //UFCR
+	{ .ptr_type = 4, .addr = 0x43F900a4, .val = 0x0000047f, }, // UBIR
+	{ .ptr_type = 4, .addr = 0x43F900a8, .val = 0x0000f423, }, // UBIR
+	{ .ptr_type = 4, .addr = 0x43F90040, .val = 0x0000000d, }, // tx
+	{ .ptr_type = 4, .addr = 0x43F90040, .val = 0x0000000a, }, // tx
+	{ .ptr_type = 4, .addr = 0x43F90040, .val = 0x00000056, }, // tx
+	{ .ptr_type = 4, .addr = 0x43F90040, .val = 0x00000050, }, // tx
+	{ .ptr_type = 4, .addr = 0x43F90040, .val = 0x00000052, }, // tx
+	{ .ptr_type = 4, .addr = 0x43F90040, .val = 0x00000032, }, // tx
+	{ .ptr_type = 4, .addr = 0x43F90040, .val = 0x00000030, }, // tx
+	{ .ptr_type = 4, .addr = 0x43F90040, .val = 0x00000030, }, // tx
+	{ .ptr_type = 4, .addr = 0x43F90040, .val = 0x0000000d, }, // tx
+	{ .ptr_type = 4, .addr = 0x43F90040, .val = 0x0000000a, }, // tx
+#endif
 	{ .ptr_type = 4, .addr = 0xB8001010, .val = 0x00000304, },
 	/* LPDDR delay line soft reset */
 	{ .ptr_type = 4, .addr = 0xB8001010, .val = 0x0000030C, },
